3be690a055a82d2cb1dca86572ab95a15f53ce70
Harikrishna.Nair <harikrishna.nair@ril.com>
2023-04-19 12:40:16 +0530

    Updated footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
 const Footer = () => {
     return (
-      <footer className="bg-dark text-white py-3">
+      <footer className="bg-dark text-white py-3 siteFooter">
         <div className="container">
-          <p className="text-center mb-0">&copy; 2023 Bill Generator</p>
+          <p className="text-center mb-0">&copy; 2023 Bill Generator. Made with <FontAwesomeIcon icon={faHeart} fixedWidth  /> by Hk.</p> 
         </div>
       </footer>
     );
