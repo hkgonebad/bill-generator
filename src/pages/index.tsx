7d754de75cd6c25e2e7f0826e7be6df8ad260bf1
awzdigital <theawzdigital@gmail.com>
2023-05-04 18:45:25 +0530

    added terms and disclaimer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,38 +27,31 @@ const HomePage = () => {
         </div>
       </section>
 
-      <Modal show={showModal === 'disclaimer'} onHide={handleClose}>
+      <Modal show={showModal === 'disclaimer'} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Disclaimer</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Disclaimer.</p>
+          <p>This bill generator is intended for personal use or testing purposes only. We do not encourage or condone any illegal or fraudulent activities, including the creation of fake bills or the use of our bill generator for such purposes. The generated bills should only be used as a digital copy for personal record keeping or as proof of purchase. We are not responsible for any misuse of the bill generator or the generated bills. By using this bill generator, you agree to use it responsibly and in compliance with all applicable laws and regulations.</p>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
 
-      <Modal show={showModal === 'terms'} onHide={handleClose}>
+      <Modal show={showModal === 'terms'} onHide={handleClose} size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>Terms</Modal.Title>
+          <Modal.Title>Terms of Use</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Terms.</p>
+        <p>Welcome to our bill generator! By accessing or using our bill generator, you agree to be bound by the following terms and conditions:</p>
+          <ul>
+            <li>The bill generator is provided for personal use or testing purposes only. You are not allowed to use the bill generator for any illegal or fraudulent activities, including the creation of fake bills.</li>
+            <li>You are solely responsible for the use of the bill generator and the generated bills. We are not responsible for any damages, losses, or liabilities arising from your use of the bill generator or the generated bills.</li>
+            <li>You are not allowed to modify, distribute, reproduce, or sell the bill generator or the generated bills without our prior written consent.</li>
+            <li>We reserve the right to modify, suspend, or terminate the bill generator at any time without prior notice.</li>
+            <li>We reserve the right to modify these terms and conditions at any time without prior notice. Your continued use of the bill generator after such modifications will constitute your acceptance of the new terms and conditions.</li>
+            <li>These terms and conditions are governed by and construed in accordance with the laws of Maharashtra. Any disputes arising from or related to the bill generator or these terms and conditions shall be subject to the exclusive jurisdiction of the courts of Maharashtra.</li>
+          </ul>
+          <p>By using our bill generator, you acknowledge that you have read, understood, and agreed to these terms and conditions. If you do not agree to these terms and conditions, you should not use the bill generator.</p>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
