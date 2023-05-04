0c5cdf865e70fc46f160053ff9530a1ad7d2d468
awzdigital <theawzdigital@gmail.com>
2023-05-04 14:39:04 +0530

    Removed unwanted text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
       <footer className="bg-dark text-white py-3 siteFooter">
         <div className="container">
-          <p className="text-center mb-0">&copy; 2023 Bill Generator. Made with <FontAwesomeIcon icon={faHeart} fixedWidth  /> by Hk.</p> 
+          <p className="text-center mb-0">&copy; 2023 Bill Generator. Made with <FontAwesomeIcon icon={faHeart} fixedWidth  /></p> 
         </div>
       </footer>
     );
